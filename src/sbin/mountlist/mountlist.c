43ef4ac4673beea13eb25068acc644d0d786c7b9
Kamil Rytarowski <n54@gmx.com>
2014-09-05 09:39:33 +0200

    mountlist: use indent for code-style sanity

--- src/sbin/mountlist/mountlist.c
+++ src/sbin/mountlist/mountlist.c
@@ -5,12 +5,12 @@
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of version 2 the GNU General Public License as
  *   published by the Free Software Foundation.
- *   
+ *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
- *   
+ *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -67,7 +67,8 @@ extern int errno;
 extern char *sys_errlist[];
 
 #define ERRMSG(err) sys_errlist[err]
-
+
+
 
 static int addVolSvcClassFlag = 0;
 static char *dmnName;
@@ -83,17 +84,19 @@ static char *workDir = "/RMVOL";
 static char *workDir = "./RMVOL";
 #endif
 static int workLock;
-static char workPathName[MAXPATHLEN+1];
-
+static char workPathName[MAXPATHLEN + 1];
+
+
 static
 void
-usage (
-       void
-       )
+usage(
+    void
+)
 {
-    fprintf (stderr, catgets(catd, 1, USAGE, "usage: %s [-v]\n"), Prog);
+	fprintf(stderr, catgets(catd, 1, USAGE, "usage: %s [-v]\n"), Prog);
 }
-
+
+
 
 /*
  * The main function returns 0 (success) if no megasafe file sets are
@@ -103,102 +106,95 @@ usage (
  * mode (-v) it also prints a list of the mounted file sets.
  */
 
-main (
-      int argc,
-      char *argv[]
-      )
-
+main(
+    int argc,
+    char *argv[]
+)
 {
 
-    int c;
-    int err;
-    int err2;
+	int c;
+	int err;
+	int err2;
 	int i;
 	int mountCnt;
 	int verboseFlag = 0;
 	int megasafeSetFound = 0;
-    extern int getopt();
-    extern char *optarg;
-    extern int optind;
-    extern int opterr;
-    struct stat stats;
+	extern int getopt();
+	extern char *optarg;
+	extern int optind;
+	extern int opterr;
+	struct stat stats;
 	struct statfs *mountTbl = NULL;
 
-    (void) setlocale(LC_ALL, "");
-    catd = catopen(MF_MOUNTLIST, NL_CAT_LOCALE);
+	(void) setlocale(LC_ALL, "");
+	catd = catopen(MF_MOUNTLIST, NL_CAT_LOCALE);
 
-    /* store only the file name part of argv[0] */
-    if ((Prog = (char *) strrchr (argv[0], '/')) == NULL) {
-        Prog = argv[0];
-    } else {
-        Prog++;
-    }
+	/* store only the file name part of argv[0] */
+	if ((Prog = (char *) strrchr(argv[0], '/')) == NULL) {
+		Prog = argv[0];
+	} else {
+		Prog++;
+	}
 
-    /* check for root */
-    if (geteuid())
-    {
-        fprintf(stderr, catgets(catd, 1, 5,
-                "\nPermission denied - user must be root to run %s.\n\n"),
-                argv[0]);
-        usage();
-        exit(1);
-    }
+	/* check for root */
+	if (geteuid()) {
+		fprintf(stderr, catgets(catd, 1, 5,
+			"\nPermission denied - user must be root to run %s.\n\n"),
+		    argv[0]);
+		usage();
+		exit(1);
+	}
+	/*
+         ** Get user-specified command line arguments.
+         */
 
-    /*
-     ** Get user-specified command line arguments.
-     */
- 
-    while ((c = getopt (argc, argv, "vV")) != EOF) {
-        switch (c) {
+	while ((c = getopt(argc, argv, "vV")) != EOF) {
+		switch (c) {
 
-          case 'v':
-		  case 'V':
-            verboseFlag = 1;
-            break;
+		case 'v':
+		case 'V':
+			verboseFlag = 1;
+			break;
 
-          default:
-            usage ();
-            exit (1);
-        }
-    }  /* end while */
+		default:
+			usage();
+			exit(1);
+		}
+	}			/* end while */
 
-    mountCnt = getfsstat (0, 0, MNT_NOWAIT);
-    if (mountCnt <= 0) {
-        fprintf (stderr, catgets(catd, 1, 2, "%s: getfsstat failed.\n"), Prog);
-        return 1;
-    }
+	mountCnt = getfsstat(0, 0, MNT_NOWAIT);
+	if (mountCnt <= 0) {
+		fprintf(stderr, catgets(catd, 1, 2, "%s: getfsstat failed.\n"), Prog);
+		return 1;
+	}
+	mountTbl = (struct statfs *) malloc(mountCnt * sizeof(struct statfs));
+	if (mountTbl == NULL) {
+		fprintf(stderr, catgets(catd, 1, 3, "%s: can't malloc file system mount table.\n"), Prog);
+		return 1;
+	}
+	err = getfsstat(mountTbl, mountCnt * sizeof(struct statfs), MNT_NOWAIT);
+	if (err < 0) {
+		fprintf(stderr, catgets(catd, 1, 2, "%s: getfsstat failed.\n"), Prog);
+		fprintf(stderr, catgets(catd, 1, 4, "%s: error = [%d] %s\n"),
+		    Prog, errno, ERRMSG(errno));
+		return 1;
+	}
+	for (i = 0; i < mountCnt; i++) {
 
-    mountTbl = (struct statfs *) malloc (mountCnt * sizeof (struct statfs));
-    if (mountTbl == NULL) {
-        fprintf (stderr, catgets(catd, 1, 3, "%s: can't malloc file system mount table.\n"), Prog);
-        return 1;
-    }
-
-    err = getfsstat (mountTbl, mountCnt * sizeof (struct statfs), MNT_NOWAIT);
-    if (err < 0) {
-        fprintf (stderr, catgets(catd, 1, 2, "%s: getfsstat failed.\n"), Prog);
-        fprintf (stderr, catgets(catd, 1, 4, "%s: error = [%d] %s\n"),
-                 Prog, errno, ERRMSG (errno));
-        return 1;
-    }
-
-    for (i = 0; i < mountCnt; i++) {
-
-		if(mountTbl[i].f_type == MOUNT_MSFS) {
+		if (mountTbl[i].f_type == MOUNT_MSFS) {
 			if (verboseFlag) {
 				if (!megasafeSetFound)
 					printf("\n");
 				printf("        %s\n", mountTbl[i].f_mntfromname);
-            }
-            megasafeSetFound = 1;
-        }
-    }
+			}
+			megasafeSetFound = 1;
+		}
+	}
 
 	if (verboseFlag && megasafeSetFound)
 		printf("\n");
 
 	return megasafeSetFound;
 
-} /* end main */
-
+}				/* end main */
 /* end mountlist.c */
